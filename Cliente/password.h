3186a5965e2f3545b112b16fc39a39bb732c618a
SaraSousa97 <saradanielafs@hotmail.com>
2017-04-18 10:07:51 +0100

    Criação do password.h

---
+++ Cliente/password.h
@@ -0,0 +1,32 @@
+#pragma once
+#include "stdio.h"
+#include "termios.h"
+
+static struct termios old, new;
+
+void initTermios(int echo){
+  
+  tcgetattr(0, &old);  
+  new = old;
+  new.c_lflag &= ~ICANON;
+  new.c_lflag &= echo ? ECHO : ~ECHO;
+  tcsetattr(0, TCSANOW, &new);
+  
+}
+
+void resetTermios(void){
+
+  tcsetattr(0, TCSANOW, &old);
+
+}
+
+char getch(void) {
+  char ch;
+  
+  initTermios(0);
+  ch = getchar();
+  resetTermios();
+  
+  return ch;
+
+}
